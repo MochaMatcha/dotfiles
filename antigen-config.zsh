b44c71af49b21bea78ed84bb25dc55ebdafb17d3
MochaMatcha <iizengamer@gmail.com>
2018-06-16 10:13:45 -0700

    added dotfiles

---
+++ antigen-config.zsh
@@ -0,0 +1,3 @@
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen theme https://github.com/denysdovhan/spaceship-prompt spaceship
---
+++ zshrc.zsh
@@ -0,0 +1,15 @@
+source ~/antigen/antigen.zsh
+
+# Antigen Plugins
+source ~/dotfiles/antigen-config.zsh && antigen apply
+
+# Spaceship Config
+
+source ~/dotfiles/spaceship-config.zsh
+
+# Aliases
+
+alias python="python3"
+alias pip="pip3"
+alias idle="idle3"
+alias la="ls -a"
